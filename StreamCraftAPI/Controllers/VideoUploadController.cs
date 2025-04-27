2d53099e1315bcb84b639852acd7a7f85388a3b4
Werterwy <orazbaijoldasbaevich@gmail.com>
2025-04-27 15:40:06 +0500

    Добавьте файлы проекта.

---
+++ StreamCraftAPI/Controllers/VideoUploadController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StreamCraftAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoUploadController : ControllerBase
+    {
+    }
+}
